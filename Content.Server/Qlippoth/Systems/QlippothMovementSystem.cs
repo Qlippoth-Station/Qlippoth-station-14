a950a5aee6c74ad27c94111665728de030879246
carmingenium <43728896+carmingenium@users.noreply.github.com>
2026-06-23 20:23:25 +0300

    movement system finished¨
    most basic working movement types are implemented both with code and as base templates to be inherited from.
    
    Non-constrained movement is not optimal, especially on space but it is functional enough for the prototype.

--- Content.Server/Qlippoth/Systems/QlippothMovementSystem.cs
+++ Content.Server/Qlippoth/Systems/QlippothMovementSystem.cs
@@ -1,4 +1,5 @@
 using Robust.Shared.GameObjects;
+using Content.Shared.Movement.Events;
 
 namespace Content.Server.Qlippoth
 {
@@ -10,6 +11,20 @@ namespace Content.Server.Qlippoth
         public override void Initialize()
         {
             base.Initialize();
+            SubscribeLocalEvent<QlippothMovementComponent, UpdateCanMoveEvent>(OnUpdateCanMove);             // to control when a qlippoth can move
+            SubscribeLocalEvent<QlippothMovementComponent, CanWeightlessMoveEvent>(OnCanWeightlessMove); // to control if it can move without being affected by gravity
+        }
+
+        private void OnUpdateCanMove(EntityUid uid, QlippothMovementComponent comp, UpdateCanMoveEvent args)
+        {
+            if (comp.MovementType == MovementType.Inanimate || comp.MovementType == MovementType.Static) // static and inanimate qlippoths are not supposed to move, just change movement type if they need to move in another way.
+                args.Cancel();
+        }
+
+        private void OnCanWeightlessMove(EntityUid uid, QlippothMovementComponent comp, ref CanWeightlessMoveEvent args) // mostly for non-constrained qlippoths, but this is a placeholder
+        {
+            if (comp.MovementType == MovementType.NonConstrained)
+                args.CanMove = true;
         }
     }
 }
